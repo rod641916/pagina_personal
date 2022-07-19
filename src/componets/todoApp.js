8509b9eac8235d012bb782f29b2277a14e78dd01
rod641916 <tapiareyesrodrigo@gmail.com>
2022-07-19 15:52:36 -0500

    Se creao carpeta componentes con todoApp.js

---
+++ src/componets/todoApp.js
@@ -0,0 +1,12 @@
+export default function TodoApp(){
+
+    //se usa jsx "permite insertar codigo html en js"//
+    //Se creal el formulario con la caja de texto y el boton
+    return <div className="todoContainer">
+        <form>
+            <input /> 
+            <input type="submit" value="Create todo"/>
+        </form>
+
+    </div>; 
+}
